31c755b9d63e520d21f17d94008877ad7a9561f8
PrinzEugen7 <nsi05162@pooae.com>
2015-11-26 15:46:06 +0900

    Create Sum.java

---
+++ Java/Basic/Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main( String[] args ) {
+		int sum = 0;
+		int[] data = { 1, 2, 3, 4, 5};
+		for( int i=0; i<data.length; i++ ) {
+			sum += data[i];
+		}
+		System.out.println(sum);
+	}
+}
