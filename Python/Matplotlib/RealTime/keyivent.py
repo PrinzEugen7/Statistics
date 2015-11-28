b8b92544d4a5b0d3506a737c2cd7fafae5fee4ea
PrinzEugen7 <nsi05162@pooae.com>
2015-11-28 13:30:20 +0900

    Create keyivent.py

---
+++ Python/Matplotlib/RealTime/keyivent.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import ctypes
+import time
+
+def getkey(key):
+    return(bool(ctypes.windll.user32.GetAsyncKeyState(key)&0x8000))
+
+def line000(pos):
+    WIDTH = 80
+    pl = [(pos)%WIDTH, (pos+1)%WIDTH, (pos+2)%WIDTH]
+    rettxt = ""
+    for i in range(WIDTH):
+        if i in pl:
+            rettxt += "0"
+        else:
+            rettxt += "-"
+    return(rettxt)
+
+
+def main():
+    ESC = 0x1B          # ESCキーの仮想キーコード
+    (x, y) = (0, 0)     # 初期値
+    plt.ion()           # 対話モードオン
+
+    while( y != 10 ):
+        line, = plt.plot(x, y, "ro",label="y=x") # (x,y)のプロット
+        line.set_ydata(y)   # y値を更新
+        plt.title("Graph")  # グラフタイトル
+        plt.xlabel("x")     # x軸ラベル
+        plt.ylabel("y")     # y軸ラベル
+        plt.legend()        # 凡例表示
+        plt.grid()          # グリッド表示
+        plt.xlim([0,10])    # x軸範囲
+        plt.ylim([0,10])    # y軸範囲
+        plt.draw()          # グラフの描画
+        plt.clf()           # 画面初期化
+        x += 1
+        y = x
+        if getkey(ESC):     # ESCキーが押されたら終了
+            break
+
+    plt.close()
+
+
+
+if __name__ == '__main__':
+    main()
