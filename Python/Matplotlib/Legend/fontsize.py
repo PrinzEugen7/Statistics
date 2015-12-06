58e3ba63f03a0cd7749132eab187c141db21ff75
PrinzEugen7 <nsi05162@pooae.com>
2015-12-06 15:31:18 +0900

    Create fontsize.py

---
+++ Python/Matplotlib/Legend/fontsize.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    x = np.linspace(0,3,4)          # xの値域(0, 1, 2, 3)
+    y = x + 1                       # 直線の式
+    plt.plot(x,y,"ro",label="y")    # 直線を引く
+    plt.legend(fontsize=40)         # 凡例(フォントサイズ40px)
+    plt.show()                      # グラフ表示
+
+if __name__ == '__main__':
+    main()
