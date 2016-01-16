26b348ffe11637e06a124efcb985ff43f23538cc
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 22:26:07 +0900

    Create ArrayLength.java

---
+++ Java/Basic/ArrayLength.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        double[] data = { 1, 2, 3, 4, 5};
+		int num = data.length;
+        System.out.println(num);
+    }
+}
+ 
