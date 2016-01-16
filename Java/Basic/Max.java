db7143c21794ff112805028f48d0139da787575c
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 18:30:41 +0900

    Create Max.java

---
+++ Java/Basic/Max.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+	public static void main( String[] args ) {
+    	float max = 0;
+		float[] data = { 1, 2, 3, 4, 5};
+		// 最大値を求める
+		for(int i=1; i < data.length;i++)
+			max = Math.max(max,data[i]);
+		System.out.println(max);
+	}
+}
