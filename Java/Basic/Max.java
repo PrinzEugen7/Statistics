380ed3442669c8078b5754c5560c169aa7c224a9
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 19:49:48 +0900

    Update Max.java

--- Java/Basic/Max.java
+++ Java/Basic/Max.java
@@ -5,8 +5,8 @@ import java.io.*;
 class Main
 {
 	public static void main( String[] args ) {
-    	float max = 0;
-		float[] data = { 1, 2, 3, 4, 5};
+    		double max = 0;
+		double[] data = { 1, 2, 3, 4, 5};
 		// 最大値を求める
 		for(int i = 0; i < data.length; i++)
 			max = Math.max(max,data[i]);
