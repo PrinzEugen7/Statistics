178eae68b02d2f5ec52a1a24dfe71402c03863dd
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 22:20:55 +0900

    Update Std.java

--- Java/Basic/Std.java
+++ Java/Basic/Std.java
@@ -6,17 +6,16 @@ class Main
 {
     public static void main( String[] args ) {
         double sum = 0;
-        double var = 0;
+        double vars = 0;
         double[] data = { 1, 2, 3, 4, 5};
-        double n = data.length;
         for(int i=0; i<data.length; i++ ) {
             sum += data[i];
         }
         double ave = ( (double)sum )/data.length;
         for (int i=0; i<data.length; i++) {
-            var += ((data[i] - ave)*(data[i] - ave));
+            vars += ((data[i] - ave)*(data[i] - ave));
         }
-       double std = Math.sqrt(var/n);
+       double std = Math.sqrt(vars/data.length);
        System.out.println(std);
     }
 }
