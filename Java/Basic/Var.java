8098ddfa0b925cf2d73a9f226f1de94d5259ffde
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 19:43:10 +0900

    Update Var.java

--- Java/Basic/Var.java
+++ Java/Basic/Var.java
@@ -4,17 +4,17 @@ import java.io.*;
  
 class Main
 {
-  public static void main( String[] args ) {
-  float sum = 0;
-  float var = 0;
-  float[] data = { 1, 2, 3, 4, 5};
-  for(int i=0; i<data.length; i++ ) {
-    sum += data[i];
-  }
-  float ave = ( (float)sum )/data.length;
-  for (int i=0; i<data.length; i++) {
-    var += ((data[i] - ave)*(data[i] - ave));
-  }
-  System.out.println(var);
-  }
+    public static void main( String[] args ) {
+    float sum = 0;
+    float var = 0;
+    float[] data = { 1, 2, 3, 4, 5};
+    for(int i=0; i<data.length; i++ ) {
+        sum += data[i];
+    }
+    float ave = ( (float)sum )/data.length;
+    for (int i=0; i<data.length; i++) {
+        var += ((data[i] - ave)*(data[i] - ave));
+    }
+    System.out.println(var);
+    }
 }
