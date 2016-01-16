ae5f7422c602d48fbc1f7dbf075b9a38fbeb2cb6
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 19:50:17 +0900

    Update Min.java

--- Java/Basic/Min.java
+++ Java/Basic/Min.java
@@ -5,8 +5,8 @@ import java.io.*;
 class Main
 {
 	public static void main( String[] args ) {
-    	float min = 100;
-		float[] data = { 1, 2, 3, 4, 5};
+    		double min = 100;
+		double[] data = { 1, 2, 3, 4, 5};
 		// 最小値を求める
 		for(int i = 0; i < data.length; i++)
 			min = Math.min(min,data[i]);
