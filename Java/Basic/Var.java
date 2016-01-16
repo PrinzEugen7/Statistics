8dd738eddc5c00902b8e82fc30870bed1e9e8c6d
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 19:41:04 +0900

    Create Var.java

---
+++ Java/Basic/Var.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+  public static void main( String[] args ) {
+  float sum = 0;
+  float var = 0;
+  float[] data = { 1, 2, 3, 4, 5};
+  for(int i=0; i<data.length; i++ ) {
+    sum += data[i];
+  }
+  float ave = ( (float)sum )/data.length;
+  for (int i=0; i<data.length; i++) {
+    var += ((data[i] - ave)*(data[i] - ave));
+  }
+  System.out.println(var);
+  }
+}
