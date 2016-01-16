6263bedf12308665161dabb8a56c115479ee60c0
PrinzEugen7 <nsi05162@pooae.com>
2016-01-16 19:48:14 +0900

    Update Average.java

--- Java/Basic/Average.java
+++ Java/Basic/Average.java
@@ -5,12 +5,12 @@ import java.io.*;
 class Main
 {
         public static void main( String[] args ) {
-                int sum = 0;
-                int[] data = { 1, 2, 3, 4, 5};
+                double sum = 0;
+                double[] data = { 1, 2, 3, 4, 5};
                 for( int i=0; i<data.length; i++ ) {
                         sum += data[i];
                 }
-                float ave = ( (float)sum )/data.length;
+                double ave = ( (double)sum )/data.length;
                 System.out.println(ave);
         }
 }
