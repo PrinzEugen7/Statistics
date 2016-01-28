f687b1fc90f368fdeb1a3d5525e2702cf4959968
PrinzEugen7 <nsi05162@pooae.com>
2016-01-28 19:41:29 +0900

    Create Exp.java

---
+++ Java/Basic/Exp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        double x = 5;
+		double ans = Math.exp(x);
+        System.out.println(ans);
+    }
+}
