ee8107353d80e34daa14bd166046139d908759c5
PrinzEugen7 <nsi05162@pooae.com>
2016-01-28 19:50:24 +0900

    Create Pow.java

---
+++ Java/Basic/Pow.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        double x = 5;
+        double y = 5;
+		double ans = Math.pow(x, y);
+        System.out.println(ans);
+    }
+}
