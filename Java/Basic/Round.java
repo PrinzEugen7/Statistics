95dcdc8b61e6d3907b845f8d22eb1c53354bb4e8
PrinzEugen7 <nsi05162@pooae.com>
2016-01-28 20:36:30 +0900

    Create Round.java

---
+++ Java/Basic/Round.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        double x = 4.6;
+		double ans = Math.round(x);
+        System.out.println(ans);
+    }
+}
