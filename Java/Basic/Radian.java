e1779deb02470b0d83783ccaf7abc560884c0984
PrinzEugen7 <nsi05162@pooae.com>
2016-01-28 20:43:00 +0900

    Create Radian.java

---
+++ Java/Basic/Radian.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+        double x = 30;
+		double ans = Math.toRadians(x);
+        System.out.println(ans);
+    }
+}
