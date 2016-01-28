9fd7000f9fe5193cb3928f06cb65f61646517fd7
PrinzEugen7 <nsi05162@pooae.com>
2016-01-28 18:27:23 +0900

    Create Tan.java

---
+++ Java/Basic/Tan.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+    public static void main( String[] args ) {
+    	double deg = 30;
+    	double rad = Math.toRadians(deg);
+		double ans = Math.tan(rad);
+        System.out.println(ans);
+    }
+}
