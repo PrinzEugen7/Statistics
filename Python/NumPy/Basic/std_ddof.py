7cf6f96f55e2304ec7a3fa56def32333425492ea
PrinzEugen7 <nsi05162@pooae.com>
2016-02-04 21:14:50 +0900

    Update std_ddof.py

--- Python/NumPy/Basic/std_ddof.py
+++ Python/NumPy/Basic/std_ddof.py
@@ -5,7 +5,7 @@ def main():
 
     data = np.array([31,30,27,25,29,34,32,31,30,29])    # データ
     std = np.std(data, ddof=1)              # 不偏標準偏差を計算
-    print(std))   # 結果を表示
+    print(std)   # 結果を表示
 
 if __name__ == '__main__':
     main()
