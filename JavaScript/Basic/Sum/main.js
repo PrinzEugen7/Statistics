e3cfffee4b56de5fa74ac7d7289bd9adebbf6f07
PrinzEugen7 <nsi05162@pooae.com>
2016-03-06 16:03:14 +0900

    Create main.js

---
+++ JavaScript/Basic/Sum/main.js
@@ -0,0 +1,15 @@
+// 平均値を計算
+function sum(data)
+{
+    var sum = 0;
+    for (i=0; i<data.length; i++) {
+      sum = sum + data[i];
+    }
+    return (sum);
+}
+
+function main()
+{
+	data = new Array(1, 2, 3, 4, 5); 
+	alert( "平均値　:　" + ave(data) );
+}
