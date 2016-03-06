8c8b4e3ea6f883d7f0767594f045e73688b297db
PrinzEugen7 <nsi05162@pooae.com>
2016-03-06 16:47:55 +0900

    Update main.js

--- JavaScript/Basic/Average/main.js
+++ JavaScript/Basic/Average/main.js
@@ -1,15 +1,22 @@
-// 平均値を計算
-function ave(data)
+// 合計値の計算
+function calc_sum(data)
 {
     var sum = 0;
     for (i=0; i<data.length; i++) {
       sum = sum + data[i];
     }
-    return (sum / data.length);
+    return (sum);
 }
 
+// 平均値の計算
+function calc_ave(data)
+{
+    return (calc_sum(data) / data.length);
+}
+
+// メイン
 function main()
 {
 	data = new Array(1, 2, 3, 4, 5); 
-	alert( "計算結果　:　" + ave(data) );
+	alert( "計算結果　:　" + calc_ave(data) );
 }
