3555b373b349c6086f26a2bda45a5061573b9565
PrinzEugen7 <nsi05162@pooae.com>
2016-03-06 16:48:54 +0900

    Update main.js

--- JavaScript/Basic/Sum/main.js
+++ JavaScript/Basic/Sum/main.js
@@ -1,5 +1,5 @@
-// 合計値を計算
-function sum(data)
+// 合計値の計算
+function calc_sum(data)
 {
     var sum = 0;
     for (i=0; i<data.length; i++) {
@@ -8,8 +8,9 @@ function sum(data)
     return (sum);
 }
 
+// メイン
 function main()
 {
 	data = new Array(1, 2, 3, 4, 5); 
-	alert( "計算結果　:　" + sum(data) );
+	alert( "計算結果　:　" + calc_sum(data) );
 }
