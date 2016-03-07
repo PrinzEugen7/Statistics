48438f338c1be4c42cbff15e5f8f7be9c6e6055a
PrinzEugen7 <nsi05162@pooae.com>
2016-03-07 17:50:38 +0900

    Update main.js

--- JavaScript/Basic/RandomRealNumber/main.js
+++ JavaScript/Basic/RandomRealNumber/main.js
@@ -1,4 +1,4 @@
-function rand_double(max, min)
+function randDouble(max, min)
 {
 	return ( Math.random() * ( ( max + 1 ) - min ) ) + min;
 }
@@ -6,6 +6,6 @@ function rand_double(max, min)
 // メイン
 function main()
 {
-	y = rand_double(10, 30);
+	y = randDouble(10, 30);
 	alert( y );
 }
