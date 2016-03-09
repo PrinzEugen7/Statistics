65fd99b80c175a0eb3c78714d10280e953fae00c
PrinzEugen7 <nsi05162@pooae.com>
2016-03-09 16:29:52 +0900

    Create main.js

---
+++ JavaScript/Probability/Distribution/Normal/main.js
@@ -0,0 +1,24 @@
+// 正規分布の計算(平均値mu, 分散sigma^2)
+var normRand = function (mu, sigma) {
+    var a = 1 - Math.random();
+    var b = 1 - Math.random();
+    var c = Math.sqrt(-2 * Math.log(a));
+    if(0.5 - Math.random() > 0) {
+        return c * Math.sin(Math.PI * 2 * b) * sigma + mu;
+    }else{
+        return c * Math.cos(Math.PI * 2 * b) * sigma + mu;
+    }
+};
+
+// メイン
+function main()
+{
+	var N = 10;	// サンプル数
+	var Y = [];
+	// サンプル数Nだけ標準正規分布を求める
+	for (var i = 0; i < N; i++) {
+		Y[i] = normRand(100, 2);
+	}
+	// 計算結果の表示
+	alert(Y);
+}
