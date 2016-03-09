4d8c5844450e899caeefac1f8e1c0f70a80e7f22
PrinzEugen7 <nsi05162@pooae.com>
2016-03-09 16:13:10 +0900

    Update main.js

--- JavaScript/Probability/Distribution/StandardNormal/main.js
+++ JavaScript/Probability/Distribution/StandardNormal/main.js
@@ -1,12 +1,12 @@
-// 正規分布の計算
-var normRand = function (m, s) {
+// 正規分布の計算(平均 値, 分散σ^2)
+var normRand = function (mu, sigma) {
     var a = 1 - Math.random();
     var b = 1 - Math.random();
     var c = Math.sqrt(-2 * Math.log(a));
     if(0.5 - Math.random() > 0) {
-        return c * Math.sin(Math.PI * 2 * b) * s + m;
+        return c * Math.sin(Math.PI * 2 * b) * sigma + mu;
     }else{
-        return c * Math.cos(Math.PI * 2 * b) * s + m;
+        return c * Math.cos(Math.PI * 2 * b) * sigma + mu;
     }
 };
 
