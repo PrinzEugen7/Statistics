86e7fa2fe9290fa4c6c44806132c951fe5d1e40f
PrinzEugen7 <nsi05162@pooae.com>
2016-06-14 22:21:02 +0900

    Create sum.cs

---
+++ CSharp/Basic/sum.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double[] data = { 5.1,4.2,3.1,2.2,1.4 };
+			double sum=0; //  合計値の格納用
+			// 合計を計算
+			for(int i = 0; i < data.Length; i++){
+				sum += data[i];
+			}
+			Console.WriteLine("合計値：" + sum);
+		}
+	}
+}
