9fccfca3ab2de8ae395abfef7bf23a1eecde3979
PrinzEugen7 <nsi05162@pooae.com>
2016-06-14 22:56:14 +0900

    Create min.cs

---
+++ CSharp/Basic/min.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double[] data = { 5.1,4.2,3.1,2.2,1.4 };
+			double max = data.Min();
+			Console.WriteLine("最小値：" + min);
+		}
+	}
+}
