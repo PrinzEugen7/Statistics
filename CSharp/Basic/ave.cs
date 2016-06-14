c5d511493d820a2a036dd1228142ed66f40f94f9
PrinzEugen7 <nsi05162@pooae.com>
2016-06-14 22:10:38 +0900

    Create ave.cs

---
+++ CSharp/Basic/ave.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] data = { 5.1,4.2,3.1,2.2,1.4 };
+            double sum=0,ave=0; //  合計値, 平均値の格納用
+            // 合計を計算
+             for(int i = 0; i < data.Length; i++){
+                 sum += data[i];
+            }
+			ave = sum / data.Length;
+            Console.WriteLine("平均値：" + ave);
+        }
+    }
+}
