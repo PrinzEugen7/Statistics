6a353f21adbbcf508135ec60ac92abd3567bc3c1
PrinzEugen7 <nsi05162@pooae.com>
2016-06-15 23:04:39 +0900

    Create Tan.cs

---
+++ CSharp/Basic/Tan.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Test
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+						double deg = 30;
+						double rad = System.Math.PI * deg / 180.0;
+						double y = System.Math.Tan(rad);
+                        Console.WriteLine("計算結果：" + y);
+                }
+        }
+}
