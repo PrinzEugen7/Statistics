d3710fc274ef2b0afb32095c235833cb80858c1a
PrinzEugen7 <nsi05162@pooae.com>
2016-06-15 21:20:01 +0900

    Create Round.cs

---
+++ CSharp/Basic/Round.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Test
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        double x = 4.6;
+                        x = System.Math.Round(x);
+                        Console.WriteLine("計算結果：" + x);
+                }
+        }
+}
