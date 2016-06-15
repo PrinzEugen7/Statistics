ae78355d41c4ff08210219d65bebd73828bbf931
PrinzEugen7 <nsi05162@pooae.com>
2016-06-15 22:49:34 +0900

    Create Exp.cs

---
+++ CSharp/Basic/Exp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Test
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        double x = 5;
+                        double y = System.Math.Exp(x);
+                        Console.WriteLine("計算結果：" + y);
+                }
+        }
+}
