d28d2224097f9911df1b3be2b82094cd3248145d
PrinzEugen7 <nsi05162@pooae.com>
2016-06-15 21:12:55 +0900

    Create Abs.cs

---
+++ CSharp/Basic/Abs.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Test
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        double x = -5.0;
+                        x = System.Math.Abs(x);
+                        Console.WriteLine("計算結果：" + x);
+                }
+        }
+}
