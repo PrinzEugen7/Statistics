be77bec8c86bace418cf3657d30ff5851cca520c
PrinzEugen7 <nsi05162@pooae.com>
2016-06-15 20:58:54 +0900

    Create ArrayLength.cs

---
+++ CSharp/Basic/ArrayLength.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Test
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        double[] data = { 5.1,4.2,3.1,2.2,1.4 };
+                        int n = data.Length;
+                        Console.WriteLine("要素数：" + n);
+                }
+        }
+}
