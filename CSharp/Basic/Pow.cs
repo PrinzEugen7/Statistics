7e2ff3bbb711e98b14d3da7b566c5a28a9cc7d8a
PrinzEugen7 <nsi05162@pooae.com>
2016-06-15 22:19:20 +0900

    Create Pow.cs

---
+++ CSharp/Basic/Pow.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Test
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        double x = 2;
+                        double y = 3;
+                        double z = System.Math.Pow(x, y);
+                        Console.WriteLine("計算結果：" + z);
+                }
+        }
+}
