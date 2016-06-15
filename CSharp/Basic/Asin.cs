d7f2d1e857ba0dd4426d60872696dd63f7b49717
PrinzEugen7 <nsi05162@pooae.com>
2016-06-15 23:07:44 +0900

    Update Asin.cs

--- CSharp/Basic/Asin.cs
+++ CSharp/Basic/Asin.cs
@@ -1,14 +1,14 @@
 using System;
 namespace Test
 {
-        class Program
+    class Program
+    {
+        static void Main(string[] args)
         {
-                static void Main(string[] args)
-                {
-						double deg = 30;
-						double rad = System.Math.PI * deg / 180.0;
-						double y = System.Math.Asin(rad);
-                        Console.WriteLine("計算結果：" + y);
-                }
+			double deg = 30;
+			double rad = System.Math.PI * deg / 180.0;
+			double y = System.Math.Asin(rad);
+					Console.WriteLine("計算結果：" + y);
         }
+    }
 }
