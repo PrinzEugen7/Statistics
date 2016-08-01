33c7738edec6114008b83f492e1cb2b4e78d20ca
PrinzEugen7 <nsi05162@pooae.com>
2016-08-01 16:53:16 +0900

    Create or.c

---
+++ C/Bool/or.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 10進数(整数)を2進数（文字列）に変換
+char *int2bin(int x)
+{
+    int i;
+    static char bin[40];
+    for(i=31; i>=0; i--){
+         bin[31-i]= ((x>>i)&1 )+'0';
+   }
+   bin[32]=0; // 文字列の最後に\0を挿入
+   return bin;
+}
+
+int main(void)
+{
+	// 10進数
+	int x = 78;
+	int y = 52;
+	// 結果表示(2進数)
+	printf(" x = %s (%d)\n",int2bin( x ), x );
+	printf(" y = %s (%d)\n",int2bin( y ), y );
+	printf("x | y = %s (%d)\n",int2bin(x|y), x|y);
+	return 0;
+}
