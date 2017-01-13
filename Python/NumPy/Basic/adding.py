ef1c1cffa4c83801d1780a315ef8b69682502b54
PrinzEugen7 <nsi05162@pooae.com>
2017-01-14 08:42:07 +0900

    Create adding.py

---
+++ Python/NumPy/Basic/adding.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+def main():
+    a = np.array([1,2,3])   # ベクトルaの生成
+    b = np.array([4,5,6])   # ベクトルbの生成
+    c = a + b               # ベクトルaとbの和
+    print(c)
+
+
+if __name__ == '__main__':
+    main()
