663f4943b03e8c1dfe67002d01dcfbb4067163cb
PrinzEugen7 <nsi05162@pooae.com>
2017-01-19 13:31:32 +0900

    Create rad2deg.py

---
+++ Python/Basic/rad2deg.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import math
+
+def main():
+
+    rad = 3.14
+    # ラジアンを度に変換
+    deg = math.degrees(rad)
+    # 結果表示
+    print(deg)
+
+
+if __name__ == "__main__":
+    main()
