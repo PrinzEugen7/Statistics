1f598c1528198d6eb633070dfccf6f899f8661ac
PrinzEugen7 <nsi05162@pooae.com>
2017-01-19 13:32:22 +0900

    Create deg2rad.py

---
+++ Python/Basic/deg2rad.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import math
+
+def main():
+
+    deg = 180
+    # 度をラジアンに変換
+    rad = math.radians(deg)
+    # 結果表示
+    print(rad)
+
+
+if __name__ == "__main__":
+    main()
