934ba5fe6bcf7742aa5390fb3abcb7f9344a36ac
PrinzEugen7 <nsi05162@pooae.com>
2017-01-20 18:47:03 +0900

    Update where.py

--- Python/NumPy/Basic/where.py
+++ Python/NumPy/Basic/where.py
@@ -4,9 +4,10 @@ import numpy as np
 def main():
 
     y = np.array([8,9,10,11,15,18,22,21,20,29])    # データ(気温)
-    y2 = y[ np.where((y>=10)&(y<20)) ]
+    x = np.where((y>=10)&(y<20))
     # 結果を表示
-    print(u"10以上20未満のデータは："+str(y2))
+    print(x[0])
+
 
 
 if __name__ == '__main__':
