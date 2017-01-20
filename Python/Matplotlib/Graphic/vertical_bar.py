6bf2af1cdc48022383e5516d518cc1765d0185fd
PrinzEugen7 <nsi05162@pooae.com>
2017-01-21 01:04:22 +0900

    Create vertical_bar.py

---
+++ Python/Matplotlib/Graphic/vertical_bar.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    x = [1, 2, 3, 4, 5]
+    y = [4, 5, 6, 7, 8]
+
+    plt.bar(x, y, align="center")           # 中央寄せで棒グラフ作成
+    plt.xticks(x, ["A","B","C", "D", "E"])  # X軸のラベル
+    plt.show()
+
+if __name__ == '__main__':
+    main()
