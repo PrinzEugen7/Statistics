c90db1cd62f0fa6a2bcc2eb84924cefd9d220c36
PrinzEugen7 <nsi05162@pooae.com>
2017-01-20 16:16:16 +0900

    Create tan.py

---
+++ Python/NumPy/Basic/tan.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def main():
+    # xの値を生成(-3.14～3.14　0.25刻み)
+    x = np.arange(-3.14, 3.14, 0.25)
+    # 関数計算
+    y = np.tan(x)
+    # 結果表示
+    print(y)
+
+if __name__ == '__main__':
+    main()
