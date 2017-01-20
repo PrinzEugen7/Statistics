a26285701a9f0730dc1aa16e0a9ddc6e7bf9b058
PrinzEugen7 <nsi05162@pooae.com>
2017-01-20 18:45:59 +0900

    Update and rename where.py to where2.py

---
+++ Python/NumPy/Basic/where2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def main():
+
+    y = np.array([8,9,10,11,15,18,22,21,20,29])    # データ(気温)
+    y2 = y[ np.where((y>=10)&(y<20)) ]
+    # 結果を表示
+    print(y2)
+
+
+if __name__ == '__main__':
+    main()
