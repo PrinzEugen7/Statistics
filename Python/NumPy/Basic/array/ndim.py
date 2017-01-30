8211877ad93c8fd8c20eab686402ced7b008bd33
PrinzEugen7 <nsi05162@pooae.com>
2017-01-31 01:27:44 +0900

    Create ndim.py

---
+++ Python/NumPy/Basic/array/ndim.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def main():
+    A = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]])
+
+    print(A.ndim)
+
+if __name__ == '__main__':
+    main()
