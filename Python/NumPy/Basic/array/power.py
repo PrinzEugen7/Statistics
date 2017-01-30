93012b6fa9c5b45b80bad4815b99ff287f44fcd7
PrinzEugen7 <nsi05162@pooae.com>
2017-01-30 23:25:14 +0900

    Create power.py

---
+++ Python/NumPy/Basic/array/power.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def main():
+    # 配列の宣言・初期化
+    A = np.array([[1, 2],[3, 4]])
+    B = A**2
+    # 画面出力
+    print(B)
+
+if __name__ == '__main__':
+    main()
