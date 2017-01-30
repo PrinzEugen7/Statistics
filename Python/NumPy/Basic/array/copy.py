b7c2cb22ff62cb98405a3fb50c7b49157f561cb2
PrinzEugen7 <nsi05162@pooae.com>
2017-01-31 00:54:29 +0900

    Update copy.py

--- Python/NumPy/Basic/array/copy.py
+++ Python/NumPy/Basic/array/copy.py
@@ -4,7 +4,9 @@ import numpy as np
 def main():
     x = np.array([1, 2, 3])
     y = x.copy()
-    print(y)
+    y = y * 2
+    print("x=", x)
+    print("y=", y)
 
 if __name__ == '__main__':
     main()
