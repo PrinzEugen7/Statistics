3096bc8253dbd45195c64fb6016ebddebbc04f82
PrinzEugen7 <nsi05162@pooae.com>
2017-01-30 23:07:05 +0900

    Update repeat.py

--- Python/NumPy/Basic/array/repeat.py
+++ Python/NumPy/Basic/array/repeat.py
@@ -4,8 +4,10 @@ import numpy as np
 def main():
     # 配列の宣言・初期化
     A = np.repeat([1, 2, 3, 4], 3)
+    B = np.array([1, 2, 3, 4]*3)
     # 画面出力
     print(A)
+    print(B)
 
 if __name__ == '__main__':
     main()
