7eee81c39390d9ad6e7fdb77bf8761470cc75034
PrinzEugen7 <nsi05162@pooae.com>
2017-01-30 23:44:58 +0900

    Create dtype.py

---
+++ Python/NumPy/Basic/array/dtype.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def main():
+    # 配列の宣言・初期化
+    A = np.array([[1, 2],[3, 4]])
+    # 画面出力
+    print(A.dtype)
+
+if __name__ == '__main__':
+    main()
