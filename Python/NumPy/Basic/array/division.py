74cd8a5ccb118fd43011339ccc6ef5a85b839221
PrinzEugen7 <nsi05162@pooae.com>
2017-01-30 23:24:07 +0900

    Create division.py

---
+++ Python/NumPy/Basic/array/division.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def main():
+    # 配列の宣言・初期化
+    A = np.array([[1, 2],[3, 4]])
+    B = np.array([[5, 6], [7, 8]])
+    C = B / A
+    # 画面出力
+    print(C)
+
+if __name__ == '__main__':
+    main()
