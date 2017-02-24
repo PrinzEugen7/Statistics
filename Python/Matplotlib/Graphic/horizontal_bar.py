69269d745dd770c8bd9d43e8e01918f965cf8d5a
PrinzEugen7 <nsi05162@pooae.com>
2017-02-24 15:42:04 +0900

    Update horizontal_bar.py

--- Python/Matplotlib/Graphic/horizontal_bar.py
+++ Python/Matplotlib/Graphic/horizontal_bar.py
@@ -6,8 +6,8 @@ def main():
     x = [1, 2, 3, 4, 5]
     y = [4, 5, 6, 7, 8]
 
-    plt.bar(x, y, align="center")           # 中央寄せで棒グラフ作成
-    plt.xticks(x, ["A","B","C", "D", "E"])  # X軸のラベル
+    plt.barh(x, y, align="center")           # 中央寄せで棒グラフ作成
+    plt.yticks(x, ["A","B","C", "D", "E"])  # X軸のラベル
     plt.show()
 
 if __name__ == '__main__':
